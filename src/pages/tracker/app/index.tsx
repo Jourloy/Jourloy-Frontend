f8d481a789f20a676483b9db394c00a6d8bc36b5
Jourloy <jourloy@yandex.ru>
2023-09-13 18:08:22 +0300

    🔨 Add footer for tracker

--- src/pages/tracker/app/index.tsx
+++ src/pages/tracker/app/index.tsx
@@ -1,6 +1,6 @@
 import {useEffect, useState} from "react";
 import {store} from "../../../store/store";
-import {Flex, Grid, Button, Card, Title, Divider, Progress, Text} from "@mantine/core";
+import {Flex, Grid, Button, Card, Title, Divider, Progress, Text, Accordion, Space} from "@mantine/core";
 import {formatter} from "../../../context";
 import TrackerLogic from "../logic";
 import IncomeModal from "./@modals/income";
@@ -9,9 +9,10 @@ import SpendModal from "./@modals/spend";
 import SpendList from "./@components/spendList.component";
 import PlannedList from "./@components/plannedList.component";
 import TrackerAPI from "../api";
-import { useNavigate } from "react-router-dom";
-import { toast } from "react-toastify";
+import {useNavigate} from "react-router-dom";
+import {toast} from "react-toastify";
 import DefaultLoading from "../../../components/loading";
+import { IconCup } from "@tabler/icons-react";
 
 export default function TrackerApp() {
 	const backend = new TrackerAPI();
@@ -187,6 +188,67 @@ export default function TrackerApp() {
 						/>
 
 						<SpendList />
+					</Grid.Col>
+
+					<Grid.Col>
+						<Divider />
+					</Grid.Col>
+
+					<Grid.Col>
+						<Accordion variant={`separated`} radius={`md`}>
+							<Accordion.Item value={`org`}>
+								<Accordion.Control>Подробнее о настройках трекера</Accordion.Control>
+								<Accordion.Panel>
+									<Text>
+										Бюджет - сумма, которая на текущий момент доступна для трат.
+										Если она не совпадает с реальным бюджет, то лучше воспользоваться
+										кнопками "доход" и "расход", чтобы синхронизировать данные, так как
+										изменение через настройки может сломать твои расчеты.
+									</Text>
+									<Space h={`xs`} />
+									<Text>
+										Дата начала отсчета - этот параметр стоит менять только после создания
+										трекера, так как он участвует в большинстве расчетов и его изменения
+										напрямую влияет на лимит денег.
+									</Text>
+								</Accordion.Panel>
+							</Accordion.Item>
+
+							<Accordion.Item value={`disabled`}>
+								<Accordion.Control>Почему некоторые кнопки не жмутся</Accordion.Control>
+								<Accordion.Panel>
+									<Text>
+										Если кнопка серая (как ниже), то значит она заблокирована.
+									</Text>
+									<Space h={`xs`} />
+									<Button fullWidth disabled>
+										Я заблокированная кнопка
+									</Button>
+									<Space h={`xs`} />
+									<Text>
+										Обычно нужно что-то сделать, чтобы ее разблокировать. Например,
+										изменить поле в настройках и тогда кнопка сохранения станет активной
+									</Text>
+									<Space h={`xs`} />
+									<Text>Но есть некоторые кнопки, над которыми я еще работаю.</Text>
+								</Accordion.Panel>
+							</Accordion.Item>
+						</Accordion>
+					</Grid.Col>
+
+					<Grid.Col>
+						<Divider />
+					</Grid.Col>
+
+					<Grid.Col>
+						<Button
+							fullWidth
+							variant={`outline`}
+							leftIcon={<IconCup stroke={1.3} />}
+							onClick={() => (window.location.href = `https://boosty.to/jourloy/donate`)}
+						>
+							Купи мне кофе
+						</Button>
 					</Grid.Col>
 				</Grid>
 			</Flex>
