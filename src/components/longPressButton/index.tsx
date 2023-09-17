c5ade3a89e0d4467c48813142cbf78caf367a427
Jourloy <jourloy@yandex.ru>
2023-09-17 23:55:21 +0300

    🔨 Fix font bug

--- src/components/longPressButton/index.tsx
+++ src/components/longPressButton/index.tsx
@@ -12,43 +12,51 @@ type TProps = {
 	seconds?: number;
 };
 
-const useStyles = createStyles((theme, {color, seconds}: {color: DefaultMantineColor, seconds: string}) => ({
-	button: {
-		position: 'relative',
-		overflow: 'hidden',
-		'&::before': {
-			display: 'flex',
-			alignItems: 'center',
-			justifyContent: 'center',
-			width: '100%',
-			height: '100%',
-			content: `attr(data-content)`,
-			position: 'absolute',
-			top: 0,
-			left: 0,
-			bottom: 0,
-			color: theme.white,
-			borderRadius: theme.radius.xs,
-			margin: 'auto',
-			clipPath: 'polygon(0 0, 0 0, 0% 100%, 0 100%)',
-			transition: `all ${seconds} ease`,
+const useStyles = createStyles(
+	(theme, {color, seconds}: {color: DefaultMantineColor; seconds: string}) => ({
+		button: {
+			position: "relative",
+			overflow: "hidden",
+			"&::before": {
+				display: "flex",
+				alignItems: "center",
+				justifyContent: "center",
+				width: "100%",
+				height: "100%",
+				content: `attr(data-content)`,
+				position: "absolute",
+				top: 0,
+				left: 0,
+				bottom: 0,
+				color: theme.white,
+				borderRadius: theme.radius.xs,
+				margin: "auto",
+				clipPath: "polygon(0 0, 0 0, 0% 100%, 0 100%)",
+				transition: `all ${seconds} ease`,
+			},
 		},
-	},
 
-	onMousedown: {
-		'&::before': {
-			backgroundColor: theme.colors[color][theme.colorScheme === 'dark' ? 5 : 6],
-			clipPath: 'polygon(0 0, 110% 0, 110% 110%, 0 110%)',
-		}
-	}
-}));
+		onMousedown: {
+			"&::before": {
+				backgroundColor: theme.colors[color][theme.colorScheme === "dark" ? 5 : 6],
+				clipPath: "polygon(0 0, 110% 0, 110% 110%, 0 110%)",
+			},
+		},
+	})
+);
 
-export default function LongPressButton({onPressed, label, seconds = 2, loading, ...props}: TProps & ButtonProps) {
+export default function LongPressButton({
+	onPressed,
+	label,
+	seconds = 2,
+	loading,
+	...props
+}: TProps & ButtonProps) {
 	const [progress, setProgress] = useState(0);
 	const callback = useCallback(() => {
 		onPressed();
-	}, [])
-    const bind = useLongPress(callback, {
+	}, []);
+	const bind = useLongPress(callback, {
 		onStart: () => setProgress(100),
 		onCancel: () => setProgress(0),
 		onFinish: () => {
@@ -57,10 +65,7 @@ export default function LongPressButton({onPressed, label, seconds = 2, loading,
 		threshold: 1000 * seconds,
 	});
 
-
-	const { classes, cx } = useStyles(
-		{color: props.color || 'gray', seconds: seconds + `s`}
-	);
+	const {classes, cx} = useStyles({color: props.color || "gray", seconds: seconds + `s`});
 
 	return (
 		<Button
@@ -68,7 +73,7 @@ export default function LongPressButton({onPressed, label, seconds = 2, loading,
 			className={cx(classes.button, {[classes.onMousedown]: progress === 100})}
 			data-content={label}
 			{...bind()}
-			loaderPosition={'center'}
+			loaderPosition={"center"}
 			loading={loading}
 		>
 			{label}
